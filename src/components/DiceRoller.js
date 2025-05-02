134ade8f5469c17b85afdf7e779a729b702fe6ca
Ioannis Emmanouil Kosmadakis <kosmadakis.john@gmail.com>
2025-05-02 19:47:32 +0300

    Light fixes

--- src/components/DiceRoller.js
+++ src/components/DiceRoller.js
@@ -111,12 +111,12 @@ const DiceRoller = () => {
       sceneRef.current.add(topLight);
       
       // Add front light
-      const frontLight = new THREE.DirectionalLight(0xffffff, 1.3);
+      const frontLight = new THREE.DirectionalLight(0xffffff, 1.5);
       frontLight.position.set(0, 5, 10);
       sceneRef.current.add(frontLight);
 
       // Add side light
-      const sideLight = new THREE.DirectionalLight(0xffffff, 1.1);
+      const sideLight = new THREE.DirectionalLight(0xffffff, 1.3);
       sideLight.position.set(10, 5, 0);
       sceneRef.current.add(sideLight);
 
